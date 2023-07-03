72a2d76ec78c980ac28113443691c67e3ae693f1
Rafiq <r@neat.af>
2023-07-03 11:30:28 -0600

    getting close to full render

--- parser.ts
+++ parser.ts
@@ -15,7 +15,12 @@ const slotDefinitions = Array.from(withoutComments.matchAll(singleBracePattern))
     const params = match[2] ? match[2].split(',').map(param => param.trim()) : [];
     const requiredParams = params.filter(param => !param.includes('='));
     const optionalParams = params.filter(param => param.includes('=')).map(param => {
-      const [name, defaultValue] = param.split('=');
+      const [name, defaultValueRaw] = param.split('=');
+      const defaultValuePattern = /"([^"\\]*(?:\\.[^"\\]*)*)"/;
+      const defaultValueMatch = defaultValueRaw.match(defaultValuePattern);
+      let defaultValue = defaultValueMatch ? defaultValueMatch[1] : defaultValueRaw;
+      // replace escaped double quote with just double quote
+      defaultValue = defaultValue.replace(/\\"/g, '"');
       return { name: name.trim(), defaultValue: defaultValue.trim() };
     });
     return { name: match[1], requiredParams, optionalParams, content: match[3].trim() };
@@ -28,12 +33,12 @@ let finalTemplate = withoutComments.replace(singleBracePattern, '').replace(/\n{
 console.log(`final template to render:\n${finalTemplate}`);
 
 // match slot variables and get params
+// TODO put this in a function so it can be called during render
 const slotPattern = /{{([^}]+)}}/g;
 const slotMatches = Array.from(finalTemplate.matchAll(slotPattern))
   .map(match => {
-    // console.log(match)
     const [name, paramsRaw] = match[1].split('(');
-    let params: any = []
+    let params: {name: string, value: string}[] = []
     if (paramsRaw) {
       let paramPattern = /(\w+)="([^"\\]*(?:\\.[^"\\]*)*)"/g;
       // let paramMatches = Array.from(paramsRaw.matchAll(paramPattern))
@@ -48,11 +53,47 @@ const slotMatches = Array.from(finalTemplate.matchAll(slotPattern))
       return {
         name,
         params: paramMatches,
+        index: match.index!,
+        length: match[0].length!
       }
     }
-    return { name, params }
-  });
-console.log('Slot matches:', slotMatches);
-console.log('snippetWithParameters params:', slotMatches[2]);
 
-// TODO replace slot variables with rendered text (combine slot variable params with default values)
+    return { name, params, index: match.index!, length: match[0].length! }
+  });
+console.log('slot variables:', slotMatches);
+// console.log('snippetWithParameters params:', slotMatches[2]);
+
+// make a copy of the final template to perform replacements
+let renderedTemplate = finalTemplate;
+
+// replace slot variables with rendered text (combine slot variable params with default values)
+// TODO need to recursively render nested slots with max depth (also contain slot params within only the namespace of the slot when rendering)
+slotMatches.reverse().forEach(slotVar => {
+  // console.log(slotVar)
+
+  // find the corresponding slot definition
+  const slotDef = slotDefinitions.find(def => def.name === slotVar.name);
+  if (!slotDef) {
+    throw new Error(`Slot definition for ${slotVar.name} not found`);
+  }
+
+  // combine slot variable params with default values
+  let params = [...slotVar.params];
+  slotDef.optionalParams.forEach(optParam => {
+    if (!params.some(param => param.name === optParam.name)) {
+      params.push({ name: optParam.name, value: optParam.defaultValue });
+    }
+  });
+
+  // replace slot variable with rendered text
+  let renderedText = slotDef.content;
+  params.forEach(param => {
+    const paramPattern = new RegExp(`{{${param.name}}}`, 'g');
+    renderedText = renderedText.replace(paramPattern, param.value);
+  });
+
+  // replace the slot variable in the template
+  renderedTemplate = renderedTemplate.substring(0, slotVar.index) + renderedText + renderedTemplate.substring(slotVar.index + slotVar.length);
+});
+
+console.log(`final rendered template:\n${renderedTemplate}`);

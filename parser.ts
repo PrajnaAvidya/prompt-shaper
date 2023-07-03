755de84f193220e565d8987718261f05cbd06f39
Rafiq <r@neat.af>
2023-07-03 11:37:48 -0600

    refactor and render nested slots

--- parser.ts
+++ parser.ts
@@ -1,9 +1,9 @@
 import { readFileSync } from 'fs'
 
-// load PromptShape file
+// load file
 // const text = readFileSync('samples/minimal.ps.txt', 'utf8')
 const text = readFileSync('samples/slots.ps.txt', 'utf8')
-console.log(`initial file:\n${text}`)
+// console.log(`initial file:\n${text}`)
 
 // remove comments
 const withoutComments = text.replace(/\/\/.*$/gm, '');
@@ -33,67 +33,62 @@ let finalTemplate = withoutComments.replace(singleBracePattern, '').replace(/\n{
 console.log(`final template to render:\n${finalTemplate}`);
 
 // match slot variables and get params
-// TODO put this in a function so it can be called during render
-const slotPattern = /{{([^}]+)}}/g;
-const slotMatches = Array.from(finalTemplate.matchAll(slotPattern))
-  .map(match => {
-    const [name, paramsRaw] = match[1].split('(');
-    let params: {name: string, value: string}[] = []
-    if (paramsRaw) {
-      let paramPattern = /(\w+)="([^"\\]*(?:\\.[^"\\]*)*)"/g;
-      // let paramMatches = Array.from(paramsRaw.matchAll(paramPattern))
-      //   .map(match => ({ name: match[1], value: match[2] }));
-      let paramMatches = Array.from(paramsRaw.matchAll(paramPattern))
-        .map(match => {
-          // replace escaped double quote with just double quote
-          let value = match[2].replace(/\\"/g, '"');
-          return { name: match[1], value: value };
-        });
+const matchSlotVariables = (template: string) => {
+  const slotPattern = /{{([^}]+)}}/g;
+  return Array.from(template.matchAll(slotPattern))
+    .map(match => {
+      const [name, paramsRaw] = match[1].split('(');
+      let params: {name: string, value: string}[] = []
+      if (paramsRaw) {
+        let paramPattern = /(\w+)="([^"\\]*(?:\\.[^"\\]*)*)"/g;
+        let paramMatches = Array.from(paramsRaw.matchAll(paramPattern))
+          .map(match => {
+            let value = match[2].replace(/\\"/g, '"');
+            return { name: match[1], value: value };
+          });
 
-      return {
-        name,
-        params: paramMatches,
-        index: match.index!,
-        length: match[0].length!
+        return {
+          name,
+          params: paramMatches,
+          index: match.index!,
+          length: match[0].length!
+        }
       }
+
+      return { name, params, index: match.index!, length: match[0].length! }
+    });
+}
+
+const renderTemplate = (template: string, depth: number = 4): string => {
+  if (depth > 5) return template; // prevent infinite recursion
+
+  let renderedTemplate = template;
+  const slotMatches = matchSlotVariables(renderedTemplate);
+
+  slotMatches.reverse().forEach(slotVar => {
+    const slotDef = slotDefinitions.find(def => def.name === slotVar.name);
+    if (!slotDef) {
+      throw new Error(`Slot definition for ${slotVar.name} not found`);
     }
 
-    return { name, params, index: match.index!, length: match[0].length! }
-  });
-console.log('slot variables:', slotMatches);
-// console.log('snippetWithParameters params:', slotMatches[2]);
+    let params = [...slotVar.params];
+    slotDef.optionalParams.forEach(optParam => {
+      if (!params.some(param => param.name === optParam.name)) {
+        params.push({ name: optParam.name, value: optParam.defaultValue });
+      }
+    });
 
-// make a copy of the final template to perform replacements
-let renderedTemplate = finalTemplate;
+    let renderedText = slotDef.content;
+    params.forEach(param => {
+      const paramPattern = new RegExp(`{{${param.name}}}`, 'g');
+      renderedText = renderedText.replace(paramPattern, param.value);
+    });
 
-// replace slot variables with rendered text (combine slot variable params with default values)
-// TODO need to recursively render nested slots with max depth (also contain slot params within only the namespace of the slot when rendering)
-slotMatches.reverse().forEach(slotVar => {
-  // console.log(slotVar)
-
-  // find the corresponding slot definition
-  const slotDef = slotDefinitions.find(def => def.name === slotVar.name);
-  if (!slotDef) {
-    throw new Error(`Slot definition for ${slotVar.name} not found`);
-  }
-
-  // combine slot variable params with default values
-  let params = [...slotVar.params];
-  slotDef.optionalParams.forEach(optParam => {
-    if (!params.some(param => param.name === optParam.name)) {
-      params.push({ name: optParam.name, value: optParam.defaultValue });
-    }
+    renderedTemplate = renderedTemplate.substring(0, slotVar.index) + renderedText + renderedTemplate.substring(slotVar.index + slotVar.length);
   });
 
-  // replace slot variable with rendered text
-  let renderedText = slotDef.content;
-  params.forEach(param => {
-    const paramPattern = new RegExp(`{{${param.name}}}`, 'g');
-    renderedText = renderedText.replace(paramPattern, param.value);
-  });
+  // recursively render nested slots
+  return renderTemplate(renderedTemplate, depth + 1);
+}
 
-  // replace the slot variable in the template
-  renderedTemplate = renderedTemplate.substring(0, slotVar.index) + renderedText + renderedTemplate.substring(slotVar.index + slotVar.length);
-});
-
-console.log(`final rendered template:\n${renderedTemplate}`);
+console.log(`final rendered template:\n${renderTemplate(finalTemplate)}`);

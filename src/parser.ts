f8f08cd170fc7a342c52e8129c3c0590e74c8aee
Rafiq <r@neat.af>
2023-07-22 15:51:16 -0600

    parsing out slots/vars

--- src/parser.ts
+++ src/parser.ts
@@ -1,8 +1,61 @@
 import { loadFileContent } from './utils'
-const parser = require('./ps-parser.js')
+import peg from "pegjs";
 
-const text = loadFileContent('samples/inline-variable-definitions.ps.txt')
+const variablesParser = peg.generate(loadFileContent('src/parsers/slots-variables.pegjs'))
 
-const test = parser.parse(text, {})
-console.log(test)
-console.log(test[7].value.params)
+/*
+parsing order
+- 1) remove comments
+- 2a) match and validate all variables and slots
+- 2b) remove variables
+- *) (future state) collection stuff
+- 3) remove excess whitespace
+- 4a) render slots with variable data from the bottom up
+- 4b) string variables will be parsed recursively (variables/slots within them will be rendered)
+
+TODO deal with stuff nested in multiline variables
+ */
+
+// const textToParse = loadFileContent('samples/inline-variable-definitions.ps.txt')
+const textToParse = loadFileContent('samples/scratch.ps.txt')
+
+// 1) remove comments using regex
+const withoutComments = textToParse.replace(/\/\/.*$/gm, '');
+console.log(`Text to parse:\n${withoutComments}`)
+
+// 2a) match all variables
+const variables: { [key: string]: object } = {}
+const slotNames: string[] = []
+// store slots/variables
+const parsed = variablesParser.parse(withoutComments)
+for (const value of parsed.parsed) {
+  console.log(value)
+  switch (value.type) {
+    case "variable":
+      if (value.variableName in variables) {
+        throw new Error(`Variable name conflict: ${value.variableName}`)
+      }
+      variables[value.variableName] = { name: value.variableName, value: value.value }
+      break
+    case "slot":
+      slotNames.push(value.variableName)
+      break
+    case "text":
+      break
+    default:
+      throw new Error(`Unknown type:\n${value}`)
+  }
+}
+// console.log('variables', variables)
+// console.log('slotNames', slotNames)
+
+const withoutVariables = parsed.text
+
+// remove excess whitespace
+const withoutExcessWhiteSpace = withoutVariables.replace(/\n{3,}/g, '\n\n').trim()
+// console.log(withoutExcessWhiteSpace)
+
+// TODO now reparse to get the correct locations of the slots
+// console.log(variablesParser.parse(withoutExcessWhiteSpace))
+
+// TODO replase all vars from the bottom up
---
+++ src/parsers/slots-variables.pegjs
@@ -0,0 +1,69 @@
+{
+  function buildText(parts) {
+    return parts.map(part => {
+      if (part.type === 'text') {
+        return part.value;
+      } else if (part.type === 'slot') {
+        let params = part.params ? "(" + part.params.map(p => p.type === 'string' ? `"${p.value}"` : p.value).join(', ') + ")" : "";
+        return "{{" + part.variableName + params + "}}";
+      } else {
+        return '';
+      }
+    }).join('');
+  }
+}
+
+start
+  = parts:part+ { return { parsed: parts, text: buildText(parts) }; }
+
+part
+  = variableDefinition
+  / slot
+  / text
+
+variableDefinition
+  = "{" _ variableName:variableName _ "}" _ content:(variableDefinition / slot / text)* _ "{/" _ variableName _ "}"
+    {
+      return { type: 'variable', variableName, content };
+    }
+  / "{" _ variableName:variableName _ "=" _ value:value _ "}"
+    {
+      return { type: 'variable', variableName, value };
+    }
+
+slot
+  = "{{" _ variableName:variableName _ "("? _ params:params? _ ")"? _ "}}"
+    {
+      return { type: 'slot', variableName, params, location: location() };
+    }
+
+params
+  = head:param tail:(_ "," _ param)*
+    { return [head].concat(tail.map(t => t[3])); }
+
+value
+  = string
+  / number
+  / functionCall
+
+string
+  = '"' chars:$[^"]* '"' { return { type: 'string', value: chars }; }
+
+number
+  = value:$([0-9]+ ("." [0-9]+)?) { return { type: 'number', value: parseFloat(value) }; }
+
+functionCall
+  = functionName:variableName "(" _ params:params _ ")" { return { type: 'function', functionName, params }; }
+
+param
+  = string
+  / number
+
+variableName
+  = first:[a-zA-Z_] rest:$[a-zA-Z_0-9]* { return first + rest; }
+
+text
+  = chars:$[^{}]+ { return { type: 'text', value: chars }; }
+
+_ "whitespace"
+  = [ \t\n\r]*

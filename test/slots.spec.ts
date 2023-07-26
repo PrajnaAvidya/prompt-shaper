981ae558782f8ed05d5797bd6be5a234357f32b8
Rafiq <r@neat.af>
2023-07-25 20:37:39 -0600

    placeholders for more tests

--- test/slots.spec.ts
+++ test/slots.spec.ts
@@ -1,33 +1,21 @@
 import { expect } from 'chai'
 import { parseTemplate } from '../src/parser'
 import { ParserVariables, ValueType } from '../src/types'
-
-// TODO make some templates to load
+import { loadFileContent } from '../src/utils'
 
 describe('slots', () => {
 	it('should correctly render a basic slot', () => {
-		const template = 'Hello, {{name}}!'
-		const variables: ParserVariables = { name: { name: 'name', type: ValueType.string, value: 'John', params: [] } }
+		const template = loadFileContent('./test/templates/slots/basic.ps.txt')
+		const variables: ParserVariables = { name: { name: 'name', type: ValueType.string, value: 'World', params: [] } }
 
 		const result = parseTemplate(template, variables)
 
-		expect(result).to.equal('Hello, John!')
+		expect(result).to.equal('Hello, World!')
 	})
 
-	it('should correctly render a slot with parameters')
+	it('should throw an error with malformed slot', () => {
+		const template = loadFileContent('./test/templates/slots/malformed.ps.txt')
 
-	it('should read slot parameters as correct types', () => {
-		const template = 'Hello, {{name}}!'
-		const variables: ParserVariables = { name: { name: 'name', type: ValueType.string, value: 'John', params: [] } }
-
-		const result = parseTemplate(template, variables, { returnParserMatches: true })
-
-		console.log(result)
+		expect(() => parseTemplate(template, {})).to.throw(`Expected "(", ")", "*", "+", "-", "/", "\\"", "}}", [0-9], [a-zA-Z_0-9], or [a-zA-Z_] but "}" found.`)
 	})
-
-	it('should throw an error with malformed slot')
-
-	it('should throw an error with invalid param')
-
-	it('should throw an error with malformed params')
 })

4f2b8ac1d8daa3ec4a5dfdfdd2729fc44a58779b
Rafiq <r@neat.af>
2023-07-29 14:59:12 -0600

    nested slot/var test

--- test/nested-tags.spec.ts
+++ test/nested-tags.spec.ts
@@ -1,1 +1,14 @@
-// TODO
+import { expect } from 'chai'
+import { parseTemplate } from '../src/parser'
+import { ParserVariables, ValueType } from '../src/types'
+import { loadFileContent } from '../src/utils'
+
+describe('nested tags', () => {
+	it('should correctly parse nested variables and slots', () => {
+		const template = loadFileContent('./test/templates/nested-tags/nested-variables.ps.txt')
+
+		const result = parseTemplate(template)
+
+		expect(result).to.equal("Outer\n\nInner")
+	})
+})

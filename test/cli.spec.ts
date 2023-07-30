e35ad2d75a36449cd169da49aa7786bf315c42e8
Rafiq <r@neat.af>
2023-07-30 14:39:21 -0600

    lint

--- test/cli.spec.ts
+++ test/cli.spec.ts
@@ -4,41 +4,41 @@ import * as path from 'path'
 import * as fs from 'fs'
 
 describe('CLI', () => {
-	it('should parse a template from a file', (done) => {
-		exec('ts-node src/cli.ts test/templates/cli/sample.ps.txt', (error, stdout, stderr) => {
+	it('should parse a template from a file', done => {
+		exec('ts-node src/cli.ts test/templates/cli/sample.ps.txt', (error, stdout) => {
 			if (error) {
-				done(error)
+				throw new Error(error.message)
 			}
 			expect(stdout.trim()).to.equal('Hello, World!')
 			done()
 		})
 	})
 
-	it('should parse a template from a string', (done) => {
-		exec('ts-node src/cli.ts "{test = \\\"hello world\\\"}{{test}}" -i', (error, stdout, stderr) => {
+	it('should parse a template from a string', done => {
+		exec('ts-node src/cli.ts "{test = \\"hello world\\"}{{test}}" -i', (error, stdout) => {
 			if (error) {
-				done(error)
+				throw new Error(error.message)
 			}
 			expect(stdout.trim()).to.equal('hello world')
 			done()
 		})
 	})
 
-	it('should save output to a file', (done) => {
+	it('should save output to a file', done => {
 		const outputPath = path.resolve(__dirname, '../output.txt')
-		exec(`ts-node src/cli.ts "{variable = \\\"hello world\\\"}{{variable}}" -i -s output.txt`, (error, stdout, stderr) => {
+		exec(`ts-node src/cli.ts "{variable = \\\"hello world\\\"}{{variable}}" -i -s output.txt`, error => {
 			if (error) {
-				done(error)
+				throw new Error(error.message)
 			}
 			expect(fs.readFileSync(outputPath, 'utf8')).to.equal('hello world')
 			done()
 		})
 	})
 
-	it('should show debug messages', (done) => {
-		exec('ts-node src/cli.ts "{variable = \\\"hello world\\\"}{{variable}}" -i -d', (error, stdout, stderr) => {
+	it('should show debug messages', done => {
+		exec('ts-node src/cli.ts "{variable = \\"hello world\\"}{{variable}}" -i -d', (error, stdout) => {
 			if (error) {
-				done(error)
+				throw new Error(error.message)
 			}
 			expect(stdout).to.include('DEBUG:')
 			done()
